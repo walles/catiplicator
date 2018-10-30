2082f4c86eed0d13089bd274364548ac48838f1f
Johan Walles <johan.walles@gmail.com>
2018-10-30 07:00:58 +0100

    Make "Level N" translateable

--- app/src/main/java/com/gmail/walles/johan/multip/LevelLaunchingActivity.java
+++ app/src/main/java/com/gmail/walles/johan/multip/LevelLaunchingActivity.java
@@ -3,7 +3,6 @@ package com.gmail.walles.johan.multip;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
 
 import java.io.IOException;
@@ -21,8 +20,9 @@ public class LevelLaunchingActivity extends AppCompatActivity {
             throw new RuntimeException("Failed to find which level to start", e);
         }
 
+        String level_n = getResources().getString(R.string.level_n);
         Button launchButton = findViewById(R.id.launch_level_button);
-        launchButton.setText("Level " + level);
+        launchButton.setText(String.format(level_n, level));
         launchButton.setOnClickListener(
                 v -> {
                     Intent intent = new Intent(LevelLaunchingActivity.this, GameActivity.class);
