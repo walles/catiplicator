ac117957d800185db63a5a2a68056e9bdaec0468
Johan Walles <johan.walles@gmail.com>
2018-10-31 06:52:58 +0100

    Add code for a credits dialog

--- app/src/main/java/com/gmail/walles/johan/multip/LevelLaunchingActivity.java
+++ app/src/main/java/com/gmail/walles/johan/multip/LevelLaunchingActivity.java
@@ -2,12 +2,57 @@ package com.gmail.walles.johan.multip;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.Button;
 
+import org.jetbrains.annotations.NonNls;
+
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+import timber.log.Timber;
 
 public class LevelLaunchingActivity extends AppCompatActivity {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.options_menu, menu);
+        menu.findItem(R.id.about).setOnMenuItemClickListener(
+                item -> {
+                    showAboutDialog();
+                    return true;
+                });
+
+        return true;
+    }
+
+    @SuppressWarnings({"resource", "IOResourceOpenedButNotSafelyClosed"})
+    private void showAboutDialog() {
+        String credits;
+        try (InputStream inputStream = getResources().openRawResource(R.raw.credits)) {
+            @NonNls final String BEGINNING_OF_INPUT = "\\A";
+            credits = new Scanner(inputStream, StandardCharsets.UTF_8.name())
+                    .useDelimiter(BEGINNING_OF_INPUT)
+                    .next();
+        } catch (IOException e) {
+            Timber.e(e, "Unable to load credits resource");
+            return;
+        }
+
+        new AlertDialog.Builder(this)
+                .setMessage(credits)
+                .setCancelable(true)
+                .setTitle(R.string.about)
+                .setNeutralButton(android.R.string.ok, (dialog, which) -> dialog.dismiss())
+                .create()
+                .show();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
